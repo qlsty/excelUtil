71d801ed8f7c0bd39341843431f2fd03cc6d5023
yt.wang <1419170877@qq.com>
2020-08-04 16:44:48 +0800

    多线程顺序输出1-100数组  * threadA 输出1 3 5 7...  * threadB 输出2 4 6 8...  * 最终按顺序输出1 2 3 4 5 6 7 8...

--- src/main/java/com/example/test/excel/thread/TestThread.java
+++ src/main/java/com/example/test/excel/thread/TestThread.java
@@ -47,7 +47,6 @@ public class TestThread {
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-
                     }
                 }
             }
